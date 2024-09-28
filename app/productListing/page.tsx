80c7f93e21575eabcf0c660626cb88605b1fce21
chiarajugno <jugnochiara@gmail.com>
2024-09-28 21:57:50 +0800

    added product listing page

---
+++ app/productListing/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { useForm } from '@mantine/form';
+import { MantineProvider, TextInput, Button, NumberInput, FileButton, Tooltip } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import { CardProduct, GroupedProduct, Product } from '@/types/types';
+import { Notifications, showNotification } from '@mantine/notifications';
+import Papa from 'papaparse';
+import CardTest from '@/components/CardTest';
+
+
+export default function ProductListing() {
+
+
+    return (
+        <p>Products</p>
+    );
+}
