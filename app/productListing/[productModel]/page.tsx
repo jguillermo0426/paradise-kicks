4378e10d0abf86fa542b6d40066bfad0f09d630f
Juliana Guillermo <jguillermo0426@gmail.com>
2024-09-29 15:47:09 +0800

    fixed eslint errors

--- app/productListing/[productModel]/page.tsx
+++ app/productListing/[productModel]/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import AdminStock from "@/components/AdminStock";
 import Footer from "@/components/Footer";
 import ProductDetails from "@/components/ProductDetails";
 import { MantineProvider, Paper } from "@mantine/core"
--- app/productListing/page.tsx
+++ app/productListing/page.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import AdminStock from "@/components/AdminStock";
 import Footer from "@/components/Footer";
-import ProductDetails from "@/components/ProductDetails";
 import ProductListing from "@/components/ProductListing";
 import { MantineProvider, Paper } from "@mantine/core"
 
