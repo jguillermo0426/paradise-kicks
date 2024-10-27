bda0b9f89f5650fbe6a1e2deab57b723dc99a382
chiarajugno <jugnochiara@gmail.com>
2024-10-27 18:21:05 +0800

    selecting sizes and displaying prices

--- components/ProductDetails.tsx
+++ components/ProductDetails.tsx
@@ -1,15 +1,31 @@
 'use client'
 import { MantineProvider} from '@mantine/core';
 import { useEffect, useState } from 'react';
-import { GroupedProduct, Product } from '@/types/types';
+import { GroupedProduct2, Product } from '@/types/types';
+import { Image, Divider, Button } from "@nextui-org/react";
 
 type ProductProps = {
     productModel: string;
 }
 
 export default function ProductDetails({productModel}: ProductProps) {
-    const [groupedProducts, setGroupedProducts] = useState<GroupedProduct[]>([]);
+    const [groupedProducts, setGroupedProducts] = useState<GroupedProduct2[]>([]);
+    const [size, setSize] = useState<string>('');
+    const [price, setPrice] = useState<number>(0);
+    const [product, setProduct] = useState<Product>();
+    const [SKU, setSKU] = useState<string>('');
+    const [stock, setStock] = useState<number>(0);
 
+    const buttonStyles = {
+        selected:{
+          border: "2px solid black"
+        },
+        unselected:{
+            border: "1px solid gray",
+        }
+    };
+
+    
     useEffect(() => {
         const getModel = async() => {
             const response = await fetch(`/api/product/get_model?model=${productModel}`, {
@@ -37,9 +53,9 @@ export default function ProductDetails({productModel}: ProductProps) {
         let colorwayId = 0;
         let sizeId = 0;
 
-        const grouped: { [Model: string]: GroupedProduct } = {};
+        const grouped: { [Model: string]: GroupedProduct2 } = {};
         products.forEach((product) => {
-            const { SKU, Model, Brand, Colorway, Size, Stock, Price } = product;
+            const { SKU, Model, Brand, Colorway, Size, Stock, Price, image_link } = product;
 
             // initialize the model if it doesnt exist
             if (!grouped[Model]) {
@@ -57,7 +73,7 @@ export default function ProductDetails({productModel}: ProductProps) {
             let colorwayGroup = grouped[Model].colorways.find(shoe => shoe.colorway === Colorway);
             // if the group of the colorways doesnt exist, create it
             if (!colorwayGroup) { 
-                colorwayGroup = { id: colorwayId, colorway: Colorway, sizes: [] };
+                colorwayGroup = { id: colorwayId, image_link: image_link, colorway: Colorway, sizes: [], model: Model, brand: Brand };
                 grouped[Model].colorways.push(colorwayGroup);
                 colorwayId += 1;
             }
@@ -68,15 +84,14 @@ export default function ProductDetails({productModel}: ProductProps) {
                 SKU: SKU,
                 size: Size,
                 stock: Stock,
-                price: Price 
+                price: Price
             });
             sizeId += 1;
         });      
         return Object.values(grouped);      
     }
 
-    /*
-    const getTotalStocks = (groupedProduct: GroupedProduct) => {
+    const getTotalStocks = (groupedProduct: GroupedProduct2) => {
         let totalStocks = 0;
         groupedProduct.colorways.forEach((colorway) => {
             colorway.sizes.forEach((size) => {
@@ -86,24 +101,106 @@ export default function ProductDetails({productModel}: ProductProps) {
 
         return totalStocks;
     }
-    */
+
+    const getLowestPrice = (groupedProduct: GroupedProduct2) => {
+        let lowestPrice = groupedProduct.colorways[0].sizes[0].price;
+        groupedProduct.colorways.forEach((colorway) => {
+            for(let i = 0; i < colorway.sizes.length; i++) {
+                if (lowestPrice > colorway.sizes[i].price) {
+                    lowestPrice = colorway.sizes[i].price
+                }
+            }
+        });
+
+        return lowestPrice;
+    }
+
+    const getHighestPrice = (groupedProduct: GroupedProduct2) => {
+        let highestPrice = groupedProduct.colorways[0].sizes[0].price;
+        groupedProduct.colorways.forEach((colorway) => {
+            for(let i = 0; i < colorway.sizes.length; i++) {
+                if (highestPrice < colorway.sizes[i].price) {
+                    highestPrice = colorway.sizes[i].price
+                }
+            }
+        });
+
+        return highestPrice;
+    }
+
+    const handleChange = (product: GroupedProduct2, sku: string) => {
+        product.colorways.forEach((colorway) => {
+            for(let i = 0; i < colorway.sizes.length; i++) {
+                if (sku === colorway.sizes[i].SKU) {
+                    setPrice(colorway.sizes[i].price);
+                    setSKU(sku);
+                    setStock(colorway.sizes[i].stock);
+                }
+            }
+        });
+    }
+
+    const startingPriceDisplay = (product: GroupedProduct2) => {
+        let lowestPrice = getLowestPrice(product);
+        let highestPrice = getHighestPrice(product);
+
+        if(lowestPrice === highestPrice){
+            return `₱${lowestPrice.toString()}`;
+        }
+        else{
+            return `₱${lowestPrice.toString()} - ₱${highestPrice.toString()}`;
+        }
+    }
+
 
     return (
         <MantineProvider>
             <div className='flex flex-col items-center m-4 relative z-50 mb-[18rem] bg-white overflow-hidden min-h-screen'>
-                <h4 className="font-bold text-large">{decodeURIComponent(productModel)}</h4>
-                <p className="font-bold">Colorways: </p>
                 {groupedProducts && 
-                    groupedProducts.map((product) => 
-                        product.colorways.map((colorway, colorwayIndex) => 
-                            <div key={colorwayIndex} className='flex flex-col justify-left'>
-                                <p className="font-bold" key={colorwayIndex}>{colorway.colorway}</p>
-                                <p>Available Sizes:</p>
-                                {colorway.sizes.map((size, sizeIndex) =>
-                                    <p key={sizeIndex}>{size.size} - {size.stock} stocks - P{size.price}</p>
-                                )}
+                    groupedProducts.map((product, productIndex) => 
+                        <div key={productIndex} className="flex flex-row justify-center w-[90%] mx-4 my-10">
+                            <div className="flex flex-col items-center w-[65%] max-w-[724px] mx-2">
+                                <Image
+                                    alt="Card background"
+                                    className="object-cover rounded-xl"
+                                    src={product.colorways[0].image_link
+                                        ? product.colorways[0].image_link
+                                        : "https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/af53d53d-561f-450a-a483-70a7ceee380f/W+NIKE+DUNK+LOW.png"
+                                    }
+                                    width={724}
+                                    height={764}
+                                    />
                             </div>
-                        )
+                            <div className="flex flex-col w-[35%] justify-start ml-6 mx-4">
+                                <p className="text-[16px] text-[#474747]" style={{ fontFamily: "Epilogue" }}>
+                                    {SKU === ''
+                                        ? getTotalStocks(product)
+                                        : stock
+                                    } stocks left
+                                </p>
+                                <p className="text-[50px]" style={{ fontFamily: "EpilogueBold", letterSpacing: "-3px", marginTop: "-6px", marginBottom: "-14px" }}>{product.model}</p>
+                                <p className="text-[20px]" style={{ fontFamily: "EpilogueMedium" }}>
+                                    {SKU === ''
+                                        ? `${startingPriceDisplay(product)}`
+                                        : `₱${price.toString()}`
+                                    }
+                                </p>
+                                {product.colorways.map((colorway, colorwayIndex) =>
+                                    <p key={colorwayIndex} className="text-[16px] text-[#474747]" style={{ fontFamily: "Epilogue" }}>{colorway.colorway}</p>
+                                )} 
+                                <Divider className="my-2" />
+                                <p className="text-[18px]" style={{ fontFamily: "Epilogue", letterSpacing: "-1px" }}>Size</p>                          
+                                <div className="w-full flex flex-wrap items-center justify-start">
+                                    {product.colorways.map((colorway) =>
+                                        colorway.sizes.map((size, sizeIndex) => (
+                                            <Button key={sizeIndex} onClick={() => handleChange(product, size.SKU)} className="w-[110px] h-[58px] flex flex-col items-center justify-center mr-2 mb-2 rounded-md" style={size.SKU === SKU ? buttonStyles.selected : buttonStyles.unselected}>
+                                                <p className="text-[16px]" style={{ fontFamily: "Epilogue" }}>{size.size}</p>
+                                            </Button>
+                                        ))
+                                    )}
+                                </div>
+                            </div>
+                        </div>
                     )
                 }
             </div>
--- components/ProductListing.tsx
+++ components/ProductListing.tsx
@@ -1,14 +1,14 @@
 'use client'
 import { SimpleGrid, MantineProvider } from '@mantine/core';
 import { useEffect, useState } from 'react';
-import { GroupedProduct, Product } from '@/types/types';
-import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
+import { GroupedProduct2, Product } from '@/types/types';
+import { Card, CardBody, Image } from "@nextui-org/react";
 import Link from 'next/link';
 
 
 
 export default function ProductListing() {
-    const [groupedProducts, setGroupedProducts] = useState<GroupedProduct[]>([]);
+    const [groupedProducts, setGroupedProducts] = useState<GroupedProduct2[]>([]);
     const brandLogoMap: Map<string, string> = new Map();
     brandLogoMap.set("Nike", "/nike.png");
     brandLogoMap.set("Adidas", "/adidas.png");
@@ -31,52 +31,53 @@ export default function ProductListing() {
         getProducts();
     }, []);
 
-
-    // groups the products based on their model, colorway, and size/stock/price
     const groupProducts = (products: Product[]) => {
         let modelId = 0;
         let colorwayId = 0;
         let sizeId = 0;
 
-        const grouped: { [Model: string]: GroupedProduct } = {};
+        const grouped: { [Model: string]: GroupedProduct2 } = {};
         products.forEach((product) => {
-            const { SKU, Model, Brand, Colorway, Size, Stock, Price } = product;
+            const { SKU, Model, Brand, Colorway, Size, Stock, Price, image_link } = product;
 
-            // initialize the model if it doesnt exist
-            if (!grouped[Model]) {
-                grouped[Model] = {
-                    id: modelId,
-                    model: Model,
-                    brand: Brand,
-                    colorways: []
-                };   
+            if(product.available){
+               // initialize the model if it doesnt exist
+                if (!grouped[Model]) {
+                    grouped[Model] = {
+                        id: modelId,
+                        model: Model,
+                        brand: Brand,
+                        colorways: []
+                    };   
+                    
+                    modelId += 1;
+                };
                 
-                modelId += 1;
-            };
-            
-            // find the shoe of the same brand with matching colorway
-            let colorwayGroup = grouped[Model].colorways.find(shoe => shoe.colorway === Colorway);
-            // if the group of the colorways doesnt exist, create it
-            if (!colorwayGroup) { 
-                colorwayGroup = { id: colorwayId, colorway: Colorway, sizes: [] };
-                grouped[Model].colorways.push(colorwayGroup);
-                colorwayId += 1;
-            }
+                // find the shoe of the same brand with matching colorway
+                let colorwayGroup = grouped[Model].colorways.find(shoe => shoe.colorway === Colorway);
+                // if the group of the colorways doesnt exist, create it
+                if (!colorwayGroup) { 
+                    colorwayGroup = { id: colorwayId, image_link: image_link, colorway: Colorway, sizes: [], model: Model, brand: Brand };
+                    grouped[Model].colorways.push(colorwayGroup);
+                    colorwayId += 1;
+                }
 
-            // assign the individual sku, size, stock, and price for the shoe
-            colorwayGroup.sizes.push({
-                id: sizeId,
-                SKU: SKU,
-                size: Size,
-                stock: Stock,
-                price: Price 
-            });
-            sizeId += 1;
+                // assign the individual sku, size, stock, and price for the shoe
+                colorwayGroup.sizes.push({
+                    id: sizeId,
+                    SKU: SKU,
+                    size: Size,
+                    stock: Stock,
+                    price: Price
+                });
+                sizeId += 1; 
+            }
+            
         });      
         return Object.values(grouped);      
     }
 
-    const getTotalStocks = (groupedProduct: GroupedProduct) => {
+    const getTotalStocks = (groupedProduct: GroupedProduct2) => {
         let totalStocks = 0;
         groupedProduct.colorways.forEach((colorway) => {
             colorway.sizes.forEach((size) => {
@@ -87,7 +88,7 @@ export default function ProductListing() {
         return totalStocks;
     }
 
-    const getTotalColors = (groupedProduct: GroupedProduct) => {
+    const getTotalColors = (groupedProduct: GroupedProduct2) => {
         let totalColors = 0;
         groupedProduct.colorways.forEach((colorway) => {
             totalColors += 1;
@@ -96,7 +97,7 @@ export default function ProductListing() {
         return totalColors;
     }
 
-    const getLowestPrice = (groupedProduct: GroupedProduct) => {
+    const getLowestPrice = (groupedProduct: GroupedProduct2) => {
         let lowestPrice = groupedProduct.colorways[0].sizes[0].price;
         groupedProduct.colorways.forEach((colorway) => {
             for(let i = 0; i < colorway.sizes.length; i++) {
@@ -112,51 +113,57 @@ export default function ProductListing() {
     return (
         <MantineProvider>
             <div className="flex flex-col items-center m-20 relative z-50 mb-[18rem] bg-white overflow-hidden min-h-screen">
-                <div className="w-full flex flex-row items-end justify-start">
-                   <p className="text-[64px]" style={{ fontFamily: "EpilogueBold", letterSpacing: "-3px", marginRight: "10px" }}>Catalogue</p> 
-                   <p className="text-[16px]" style={{ fontFamily: "EpilogueBold", letterSpacing: "-1px", paddingBottom: "23px" }}>All items</p> 
+                <div className="flex flex-col items-start justify-start w-[1500px]">
+                    <div className="w-full flex flex-row items-end justify-start ml-4">
+                        <p className="text-[64px]" style={{ fontFamily: "EpilogueBold", letterSpacing: "-3px", marginRight: "10px" }}>Catalogue</p> 
+                        <p className="text-[16px]" style={{ fontFamily: "EpilogueBold", letterSpacing: "-1px", marginRight: "auto", paddingBottom: "23px" }}>All items</p> 
+                    </div>
+                    <div className="w-full flex flex-wrap items-center justify-start">
+                    {groupedProducts && 
+                        groupedProducts.map((product, productIndex) => 
+                            <Link key={productIndex} href={`/product-details/${product.model}`}>
+                                <Card key={productIndex} className="w-full max-w-[300px] h-[488px] max-h-[488px] flex flex-col items-center border-[1px] border-black rounded-2xl mx-4 mb-8 p-8">
+                                    <CardBody>
+                                        <div className="flex flex-col items-center">
+                                            <Image
+                                                alt="Card background"
+                                                className="object-cover rounded-xl"
+                                                src={product.colorways[0].image_link
+                                                    ? product.colorways[0].image_link
+                                                    : "https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/af53d53d-561f-450a-a483-70a7ceee380f/W+NIKE+DUNK+LOW.png"
+                                                }
+                                                height={250}
+                                                width={250} />
+                                        </div>
+                                        <div className="h-full flex flex-col items-between justify-between">
+                                            <div className="flex flex-col items-start">
+                                                <small className="mt-4 text-[14px] text-[#808080]" style={{ fontFamily: "Epilogue" }}>{getTotalColors(product)}
+                                                    { getTotalColors(product) > 1 
+                                                        ? " colors"
+                                                        : " color"
+                                                    } 
+                                                </small>
+                                                {/*product.colorways.map((colorway, colorwayIndex) => 
+                                                    <small key={colorwayIndex} className="text-default-500">{colorway.colorway}</small>
+                                                )*/}
+                                                <p className="text-[16px]" style={{ fontFamily: "EpilogueSemiBold", letterSpacing: "-0.5px" }}>{product.model}</p>  
+                                                <p className="text-[16px]" style={{ fontFamily: "Epilogue", letterSpacing: "-0.5px"  }}>&#8369; {getLowestPrice(product)}</p>
+                                            </div>
+                                            
+                                            <div className="flex flex-row justify-between items-end">
+                                                <p className="text-[16px]" style={{ fontFamily: "Epilogue", letterSpacing: "-0.5px"  }}>{getTotalStocks(product)} stocks left</p>
+                                                <Image 
+                                                    src={brandLogoMap.get(product.brand)}
+                                                    height={26}/>
+                                            </div>
+                                        </div>
+                                    </CardBody>
+                                </Card>
+                            </Link>
+                    )}
+                    </div>
                 </div>
-                <SimpleGrid cols={4} className="w-full flex flex-row justify-between mb-20" >
-                {groupedProducts && 
-                    groupedProducts.map((product, productIndex) => 
-                        <Link key={productIndex} href={`/product-details/${product.model}`}>
-                            <Card key={productIndex} className="w-full max-w-[300px] h-[480px] flex flex-col items-center border-[1px] border-black rounded-2xl p-8">
-                                <CardBody className="w-full h-full overflow-visible">
-                                    <div className="flex flex-col items-center">
-                                        <Image
-                                            alt="Card background"
-                                            className="object-cover rounded-xl"
-                                            src={"https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/af53d53d-561f-450a-a483-70a7ceee380f/W+NIKE+DUNK+LOW.png"}
-                                            height={250}
-                                            width={250} />
-                                    </div>
-                                    <div className="h-full flex flex-col items-between justify-between">
-                                        <div className="flex flex-col items-start">
-                                            <small className="mt-4 text-[14px] text-[#808080]" style={{ fontFamily: "Epilogue" }}>{getTotalColors(product)}
-                                                { getTotalColors(product) > 1 
-                                                    ? " colors"
-                                                    : " color"
-                                                } 
-                                            </small>
-                                            {/*product.colorways.map((colorway, colorwayIndex) => 
-                                                <small key={colorwayIndex} className="text-default-500">{colorway.colorway}</small>
-                                            )*/}
-                                            <p className="text-[16px]" style={{ fontFamily: "EpilogueSemiBold", letterSpacing: "-0.5px" }}>{product.model}</p>  
-                                            <p className="text-[16px]" style={{ fontFamily: "Epilogue", letterSpacing: "-0.5px"  }}>&#8369; {getLowestPrice(product)}</p>
-                                        </div>
-                                        
-                                        <div className="flex flex-row justify-between items-end">
-                                            <p className="text-[16px]" style={{ fontFamily: "Epilogue", letterSpacing: "-0.5px"  }}>{getTotalStocks(product)} stocks left</p>
-                                            <Image 
-                                                src={brandLogoMap.get(product.brand)}
-                                                height={26}/>
-                                        </div>
-                                    </div>
-                                </CardBody>
-                            </Card>
-                        </Link>
-                )}
-                </SimpleGrid>
+                
             </div>
         </MantineProvider>
     );
