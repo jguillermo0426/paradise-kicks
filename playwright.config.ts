a26b2bd18523a2170e5ae21a1891f4bfa24f1936
rencebry <clarence_borlaza@dlsu.edu.ph>
2024-11-26 23:59:55 +0800

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,12 +71,12 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // running login page to test admin dashboard
-  // webServer: {
-  //   command: 'npm run dev',
-  //   url: 'http://localhost:3000/',
-  //  timeout: 120000,
-  //   reuseExistingServer: !process.env.CI,
-  // },
+ 
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000/',
+   timeout: 120000,
+    reuseExistingServer: !process.env.CI,
+  },
    
 });
