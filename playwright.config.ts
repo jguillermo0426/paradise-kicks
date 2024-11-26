e57a3ba3dc70ac11c4de3ca6929590ec5e5ac755
rencebry <clarence_borlaza@dlsu.edu.ph>
2024-11-27 00:25:32 +0800

    Update CI config of playwright (might have to test pipeline)

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,15 +39,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -74,7 +74,7 @@ export default defineConfig({
  
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:3000/',
+    url: 'http://localhost:3000/login',
    timeout: 120000,
     reuseExistingServer: !process.env.CI,
   },
